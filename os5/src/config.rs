a69dddb6341fea9a11b8dba91d7e4db82acf7eb0
fengF3ng <1711623223@qq.com>
2022-07-22 06:41:18 +0800

    fix bug by correct the BIT_STRIDE

--- os5/src/config.rs
+++ os5/src/config.rs
@@ -12,4 +12,4 @@ pub const TRAMPOLINE: usize = usize::MAX - PAGE_SIZE + 1;
 pub const TRAP_CONTEXT: usize = TRAMPOLINE - PAGE_SIZE;
 pub const CLOCK_FREQ: usize = 12500000;
 
-pub const BIG_STRIDE: isize = (isize::MAX)/2;
+pub const BIG_STRIDE: isize = i8::MAX as isize;
